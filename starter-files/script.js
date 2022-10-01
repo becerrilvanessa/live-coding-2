dceace84a621f70a8bb63e4a4b8c0f5b96f91351
Vanessa Becerril Hernandez <becerrilhernandezv097@gmail.com>
2022-09-30 21:12:24 -0500

    se agrega ejercicio 2

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,13 +1,38 @@
-// API
-const API_ENDPOINT = 'https://yesno.wtf/api';
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const name = document.querySelector('name');
+const blog = document.querySelector('#blog');
+const loc = document.querySelector('.location');
 
-/**
- * STEPS:
- *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
- *
- */
+const displayUser = async () => {
+
+
+  try {
+
+
+    const response = await fetch("https://api.github.com");
+  console.log(response);
+  `name.textContent = 'cargando...';
+  name.textContent = '${data.name}';
+  blog.textContent = '${data.blog}';
+  loc.textContent = '${data.location}';`
+
+  let data = await response.json();
+       console.log(data);
+
+    
+  } catch (error) {
+
+    function handleError(err) {
+      console.log('OH NO!');
+      console.log(err);
+      `name.textContent = Algo salió mal: ${err}`
+    
+  }
+  
+}
+
+
+}
+
+displayUser('stolinski').catch(handleError);
